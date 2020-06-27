0df0dc1e0656b72a62e11be01a4a5ed0eedce492
soleme <soleme@gmail.com>
2020-06-27 23:03:46 +0900

    postman 데이터 넣기

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+// application/json type
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://soleme:abc1234@boilerplate-qmjfi.mongodb.net/boilerplate?retryWrites=true&w=majority', {
     useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
@@ -10,5 +18,19 @@ mongoose.connect('mongodb+srv://soleme:abc1234@boilerplate-qmjfi.mongodb.net/boi
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req,res) => {
+    // 회원가입 할떄 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err,userInfo)=>{
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
